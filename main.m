91e7a2fd3ae6177c913859dc23a6efc132c0b91d
Dave Dribin <dave@dribin.org>
2009-01-13 09:26:32 -0600

    Initial import

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  CustomReturnID
+//
+//  Created by Dave Dribin on 1/13/09.
+//  Copyright Bit Maki, Inc. 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
